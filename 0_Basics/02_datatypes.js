04fce8a86a2c323a6cfd4dd74ef5a71d86d66877
Sakshamgupta257 <guptasaksham.257@gmail.com>
2024-02-21 12:00:58 +0530

    added 02_datatypes

---
+++ 0_Basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS code as newer version
+
+//alert("hello") // we are using nodejs, not browser
+
+console.log(3+3); console.log("Hitesh") // code readabiliity should be high
+
+let name = "hitesh" //string
+let age = 18 //number
+let isLoggedIn = false //bool
+
+
+//number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => uniqueness
+
+
+// object
+
+//typeof is used to know the type od data
+console.log(typeof "hitesh");
+
+// type of null is object
+console.log(typeof null);
+
+//type of undefined is undefined
+console.log(typeof undefined);
